fba0c183a86f7877e8798af77718ff6b8d5164c7
pranikasudhakar <85849436+pranikasudhakar@users.noreply.github.com>
2021-06-29 00:05:52 -0400

    Delete week3/day3 directory

--- week3/day3/Hardwares.java
+++
@@ -1,6 +0,0 @@
-package week3.day3;
-
-public interface Hardwares {
-	public void hardwareResources();
-
-}
--- week3/day3/Software.java
+++
@@ -1,5 +0,0 @@
-package week3.day3;
-
-public interface Software {
-	public void softwareResources();
-}
